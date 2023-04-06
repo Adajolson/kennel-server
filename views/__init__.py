fbb6677b2f5cccd861de4aea60460848316cca6a
Adam Olson <adajolson@yahoo.com>
2023-04-06 14:38:32 -0600

    book work chapter 1-5

--- views/__init__.py
+++ views/__init__.py
@@ -1,1 +1,7 @@
-from .animal_requests import get_all_animals
+from .animal_requests import get_all_animals, get_single_animal, create_animal, delete_animal
+from .location_requests import get_all_locations, get_single_location
+from .location_requests import create_location, delete_location
+from .employee_requests import get_all_employees, get_single_employee
+from .employee_requests import create_employee, delete_employee
+from .customer_requests import get_all_customers, get_single_customer
+from .customer_requests import create_customer, delete_customer
---
+++ views/customer_requests.py
@@ -0,0 +1,63 @@
+CUSTOMERS = [
+    {
+        "id": 1,
+        "name": "Ryan Tanay"
+    }
+]
+
+def get_all_customers():
+    """This function gets all customers
+    """
+    return CUSTOMERS
+
+# Function with a single parameter
+def get_single_customer(id):
+    """This function gets a single customer
+    """
+    # Variable to hold the found customer, if it exists
+    requested_customer = None
+
+    # Iterate the CUSTOMERS list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for customer in CUSTOMERS:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if customer["id"] == id:
+            requested_customer = customer
+
+    return requested_customer
+
+def create_customer(customer):
+    """this function goes in the POST function in the request handler module
+    """
+    # Get the id value of the last customer in the list
+    max_id = CUSTOMERS[-1]["id"]
+
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an `id` property to the customer dictionary
+    customer["id"] = new_id
+
+    # Add the customer dictionary to the list
+    CUSTOMERS.append(customer)
+
+    # Return the dictionary with `id` property added
+    return customer
+
+def delete_customer(id):
+    """this function will delete customer from the dictionary
+    """
+    # Initial -1 value for customer index, in case one isn't found
+    customer_index = -1
+
+    # Iterate the CUSTOMERS list, but use enumerate() so that you
+    # can access the index value of each item
+    for index, customer in enumerate(CUSTOMERS):
+        if customer["id"] == id:
+            # Found the customer. Store the current index.
+            customer_index = index
+
+    # If the customer was found, use pop(int) to remove it from list
+    if customer_index >= 0:
+        CUSTOMERS.pop(customer_index)
