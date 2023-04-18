d0c16ea6d66ad6f67cbabba33b422b2b70917b9b
Adam Olson <adajolson@yahoo.com>
2023-04-18 14:54:25 -0600

    implementing sql into Kennels

--- views/__init__.py
+++ views/__init__.py
@@ -1,8 +1,8 @@
-from .animal_requests import get_all_animals, get_single_animal, create_animal
-from .animal_requests import delete_animal, update_animal
+from .animal_requests import get_all_animals, get_single_animal, create_animal, get_animals_by_location_id
+from .animal_requests import delete_animal, update_animal, get_animals_by_status
 from .location_requests import get_all_locations, get_single_location, update_location
 from .location_requests import create_location, delete_location
 from .employee_requests import get_all_employees, get_single_employee, update_employee
-from .employee_requests import create_employee, delete_employee
+from .employee_requests import create_employee, delete_employee, get_employees_by_location_id
 from .customer_requests import get_all_customers, get_single_customer, update_customer
-from .customer_requests import create_customer, delete_customer
+from .customer_requests import create_customer, delete_customer, get_customers_by_email
